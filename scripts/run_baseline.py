545e185899d83aaa61bcf8707c3cf2656a423b81
Saket Pathak <saketpathakitsme@gmail.com>
2026-04-11 20:33:46 +0530

    some file changes are made to better resolve the issue

--- scripts/run_baseline.py
+++ scripts/run_baseline.py
@@ -1,5 +1,11 @@
 from agent.baseline_openai import run
 
-if __name__ == "__main__":
+#  This is what validator will call
+def run_entry():
     for level in ["easy", "medium", "hard"]:
         run(level)
+
+
+# Optional: still allow local execution
+if __name__ == "__main__":
+    run_entry()
