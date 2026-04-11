4f6ce74c6ad9581b2b66bf1fab9da20d988ce1a1
Saket Pathak <saketpathakitsme@gmail.com>
2026-04-12 00:58:32 +0530

    fix: add root endpoint

--- server/app.py
+++ server/app.py
@@ -4,5 +4,10 @@ from env.models import Action, Observation
 from server.environment import ComplaintEnvironment
 
 app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Naarad OpenEnv is running"}
+
 server = HTTPEnvServer(ComplaintEnvironment, Action, Observation)
 server.register_routes(app)
